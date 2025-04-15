7f9416bebcdf49d5dcc5139f443b5ca8c7f3360a
Никита Смирнов <152095229+N1smi@users.noreply.github.com>
2025-04-15 20:51:57 +0300

    Constructors, destructor, basic getters and setters are implemented, and the square bracket operator is overloaded.

--- Online_Game_Store_Project/TVector/my_vector.h
+++ Online_Game_Store_Project/TVector/my_vector.h
@@ -12,12 +12,12 @@ enum State { empty, busy, deleted };
 template <class T>
 class TVector {
     T* _data;
-    size_t _capacity;
     size_t _size;
+    size_t _capacity;
     size_t _deleted;
     State* _states;
 public:
-    TVector(size_t size = 0, const T* data = nullptr);
+    TVector(size_t size = 0);
     TVector(std::initializer_list<T>);
     TVector(const TVector<T>& other);
     ~TVector();
@@ -25,6 +25,10 @@ public:
     inline bool is_empty() const noexcept;
 
     inline T* data() noexcept;
+    inline T& front();
+    inline T& back();
+    inline T* begin() noexcept;
+    inline T* end() noexcept;
 
     inline const T* data() const noexcept;
     inline size_t size() const noexcept;
@@ -44,8 +48,8 @@ public:
     void erase(size_t pos);
     void erase(size_t first, size_t last);
 
-    template <typename... Args>
-    void emplace(size_t pos, Args&&... args);
+    void replace(size_t pos, const T& new_value);
+    void replace(T* pos, const T& new_value);
 
     void assign(size_t size, const T& value);
     inline const T& at(size_t pos) const;
@@ -59,13 +63,15 @@ public:
     TVector<T>& operator=(const TVector<T>& other);
     bool operator==(const TVector<T>& other) const;
     bool operator!=(const TVector<T>& other) const;
-    inline const T& operator[](size_t pos) const;
+
+    const T& operator[](size_t pos) const;
+    T& operator[](size_t pos);
 
     friend void shuffle(TVector<T>& vec);
     friend void sort(TVector<T>& vec);
-    friend size_t find_first(const TVector<T>& vec, const T& value);
-    friend size_t find_last(const TVector<T>& vec, const T& value);
-    friend TVector<size_t> find(const TVector<T>& vec, const T& value);
+    friend T* find_first(const TVector<T>& vec, const T& value);
+    friend T* find_last(const TVector<T>& vec, const T& value);
+    friend TVector<T*> find(const TVector<T>& vec, const T& value);
 private:
     inline bool is_full() const noexcept;
     void reallocate();
@@ -75,5 +81,111 @@ private:
     friend void swap(T* first_elem, T* second_elem);
     friend void reallocate_for_sort(TVector<T>& vec);
 };
+
+template <class T>
+TVector<T>::TVector(size_t size) {
+    _size = size;
+    _capacity = _size;
+    _deleted = 0;
+    _data = (_capacity == 0) ? nullptr : new T[_capacity];
+    _states = (_capacity == 0) ? nullptr : new State[_capacity];
+
+    for (size_t i = 0; i < _size; i++) {
+        _data[i] = T();
+        _states[i] = busy;
+    }
+}
+
+template <class T>
+TVector<T>::TVector(std::initializer_list<T> init_list) {
+    _size = init_list.size();
+    _capacity = _size;
+    _deleted = 0;
+    _data = (_capacity == 0) ? nullptr : new T[_capacity];
+    _states = (_capacity == 0) ? nullptr : new State[_capacity];
+
+    if (_capacity != 0) {
+        size_t i = 0;
+        for (const auto& item : init_list) {
+            _data[i] = item;
+            _states[i] = busy;
+            i++;
+        }
+    }
+}
+
+template <class T>
+TVector<T>::TVector(const TVector<T>& other) {
+    _size = other._size;
+    _capacity = _size;
+    _deleted = 0;
+    _data = (_capacity == 0) ? nullptr : new T[_capacity];
+    _states = (_capacity == 0) ? nullptr : new State[_capacity];
+
+    if (_capacity != 0) {
+        size_t j = 0;
+        for (size_t i = 0; i < other._capacity; i++) {
+            if (other._states[i] == busy) {
+                _data[j] = other._data[i];
+                _states[j] = busy;
+                j++;
+            }
+        }
+    }
+}
+
+template <class T>
+TVector<T>::~TVector() {
+    delete[] _data;
+    delete[] _states;
+}
+
+template <class T>
+inline bool TVector<T>::is_empty() const noexcept {
+    return _size == 0;
+}
+
+template <class T>
+inline T* TVector<T>::data() noexcept {
+    return _data;
+}
+
+template<class T>
+inline const T* TVector<T>::data() const noexcept {
+    return _data;
+}
+
+template<class T>
+inline size_t TVector<T>::size() const noexcept {
+    return _size;
+}
+
+template<class T>
+inline size_t TVector<T>::capacity() const noexcept {
+    return _capacity;
+}
+
+template<class T>
+const T& TVector<T>::operator[](size_t pos) const {
+    if (pos >= _size) {
+        throw std::logic_error("Index out of range");
+    }
+
+    size_t busy_count = 0;
+    for (size_t i = 0; i < _size; i++) {
+        if (_states[i] == busy) {
+            if (busy_count == pos)
+            {
+                return _data[i];
+            }
+            busy_count++;
+        }
+    }
+}
+
+template<class T>
+T& TVector<T>::operator[](size_t pos) {
+    return const_cast<T&>(std::as_const(*this)[pos]);
+}
 
 #endif  // ONLINE_GAME_STORE_PROJECT_MY_VECTOR_H_
--- Online_Game_Store_Project/Test_Application/main.cpp
+++ Online_Game_Store_Project/Test_Application/main.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <windows.h>
+#include "my_vector.h"
 #include "User.h"
 #include "Administrator.h"
 #include "Client.h"
@@ -73,6 +74,108 @@ namespace TestSystem {
     }
 };
 
+bool test_1_try_create_default_vector() {
+    bool expected_result = true;
+    bool actual_result = true;
+    try {
+        TVector<Client> vec_1();
+    }
+    catch (const std::exception& ex) {
+        actual_result = false;
+    }
+    return TestSystem::check(expected_result, actual_result);
+}
+
+bool test_2_try_create_vector_with_initialization() {
+    bool expected_result = true;
+    bool actual_result = true;
+    try {
+        TVector<int> vec_1(5);
+    }
+    catch (const std::exception& ex) {
+        actual_result = false;
+    }
+    return TestSystem::check(expected_result, actual_result);
+}
+
+bool test_3_check_basic_values_in_initialization() {
+    size_t test_size = 3;
+    const char basic = '\0';
+    TVector<char> vec_1(test_size);
+    bool result = (TestSystem::check(test_size, vec_1.size())) && (TestSystem::check(test_size, vec_1.capacity())) &&
+        (TestSystem::check(basic, vec_1[0])) && (TestSystem::check(basic, vec_1[1])) && 
+        (TestSystem::check(basic, vec_1[2]));
+    return result;
+}
+
+bool test_4_throw_when_try_applying_for_position_abroad() {
+    TVector<double> vec_1(100);
+    bool expected_result = false;
+    bool actual_result = true;
+    try {
+        double error = vec_1[100];
+    }
+    catch (const std::exception& ex) {
+        actual_result = false;
+    }
+    return TestSystem::check(expected_result, actual_result);
+}
+
+bool test_5_try_create_vector_with_initialization_list() {
+    bool expected_result = true;
+    bool actual_result = true;
+    try {
+        TVector<float> vec_1({ 1.0f, 0.0f, 2.356f });
+        TVector<char> vec_2({});
+    }
+    catch (const std::exception& ex) {
+        actual_result = false;
+    }
+    return TestSystem::check(expected_result, actual_result);
+}
+
+bool test_6_check_values_in_initialization_list() {
+    size_t test_size = 3;
+    const char value_1 = '5';
+    const char value_2 = '\0';
+    const char value_3 = '$';
+    TVector<char> vec_1({'5', '\0', '$'});
+    bool result = (TestSystem::check(test_size, vec_1.size())) && (TestSystem::check(test_size, vec_1.capacity())) &&
+        (TestSystem::check(value_1, vec_1[0])) && (TestSystem::check(value_2, vec_1[1])) &&
+        (TestSystem::check(value_3, vec_1[2]));
+    return result;
+}
+
+bool test_7_try_create_vector_with_copy() {
+    bool expected_result = true;
+    bool actual_result = true;
+    TVector<int> vec_1(5);
+    try {
+        TVector<int> vec_2(vec_1);
+    }
+    catch (const std::exception& ex) {
+        actual_result = false;
+    }
+    return TestSystem::check(expected_result, actual_result);
+}
+
+bool test_8_check_values_in_copy() {
+    TVector<int> vec_1({ -1, 2, 3 });
+    TVector<int> vec_2(vec_1);
+    bool result = (TestSystem::check(vec_1.size(), vec_2.size())) && (TestSystem::check(vec_1.capacity(), vec_2.capacity())) &&
+        (TestSystem::check(vec_1[0], vec_2[0])) && (TestSystem::check(vec_1[1], vec_2[1])) &&
+        (TestSystem::check(vec_1[2], vec_2[2]));
+    return result;
+}
+
+bool test_9_check_is_empty() {
+    TVector<char> vec_1;
+    bool expected_result = true;
+    bool actual_result = vec_1.is_empty();
+
+    return TestSystem::check(expected_result, actual_result);
+}
+
 int main() {
     User user_1;
     Client client_1;
@@ -80,8 +183,21 @@ int main() {
     Game game_1;
     Feedback feedback_1;
     GameStore gamestore_1;
-    // TestSystem::start_test(test_num_name, " Class.test_num_name");
-    // TestSystem::print_final_info();
-    // system("pause");
+
+    TestSystem::start_test(test_1_try_create_default_vector, " TVector.test_1_try_create_default_vector");
+    TestSystem::start_test(test_2_try_create_vector_with_initialization, " TVector.test_2_try_create_vector_with_initialization");
+    TestSystem::start_test(test_3_check_basic_values_in_initialization, " TVector.test_3_check_basic_values_in_initialization");
+    TestSystem::start_test(test_4_throw_when_try_applying_for_position_abroad, 
+        " TVector.test_4_throw_when_try_applying_for_position_abroad");
+    TestSystem::start_test(test_5_try_create_vector_with_initialization_list,
+        " TVector.test_5_try_create_vector_with_initialization_list");
+    TestSystem::start_test(test_6_check_values_in_initialization_list,
+        " TVector.test_6_check_values_in_initialization_list");
+    TestSystem::start_test(test_7_try_create_vector_with_copy, " TVector.test_7_try_create_vector_with_copy");
+    TestSystem::start_test(test_8_check_values_in_copy, " TVector.test_8_check_values_in_copy");
+    TestSystem::start_test(test_9_check_is_empty, " TVector.test_9_check_is_empty");
+
+    TestSystem::print_final_info();
+    system("pause");
     return 0;
 }
