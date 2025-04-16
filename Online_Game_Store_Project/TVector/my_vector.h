bc66b6a1a2ef990c6b7ccc25804e8cad428c3460
Никита Смирнов <152095229+N1smi@users.noreply.github.com>
2025-04-16 20:36:23 +0300

    Fixing errors, adding reallocate, basic inserts, all minor getters and setters.

--- Online_Game_Store_Project/TVector/my_vector.h
+++ Online_Game_Store_Project/TVector/my_vector.h
@@ -72,9 +72,12 @@ public:
     friend T* find_first(const TVector<T>& vec, const T& value);
     friend T* find_last(const TVector<T>& vec, const T& value);
     friend TVector<T*> find(const TVector<T>& vec, const T& value);
+
 private:
+    size_t real_pos(size_t pos);
+    T* real_address(size_t pos);
     inline bool is_full() const noexcept;
-    void reallocate();
+    void reallocate(size_t new_size);
     void reallocate_for_deleted();
 
     friend int rand_generation(int min, int max);
@@ -104,13 +107,11 @@ TVector<T>::TVector(std::initializer_list<T> init_list) {
     _data = (_capacity == 0) ? nullptr : new T[_capacity];
     _states = (_capacity == 0) ? nullptr : new State[_capacity];
 
-    if (_capacity != 0) {
-        size_t i = 0;
-        for (const auto& item : init_list) {
-            _data[i] = item;
-            _states[i] = busy;
-            i++;
-        }
+    size_t i;
+    auto it = init_list.begin();
+    for (i = 0; it != init_list.end(); i++, it++) {
+         _data[i] = *it;
+        _states[i] = busy;
     }
 }
 
@@ -123,15 +124,16 @@ TVector<T>::TVector(const TVector<T>& other) {
     _states = (_capacity == 0) ? nullptr : new State[_capacity];
 
     if (_capacity != 0) {
-        size_t j = 0;
+        size_t busy_count = 0;
         for (size_t i = 0; i < other._capacity; i++) {
             if (other._states[i] == busy) {
-                _data[j] = other._data[i];
-                _states[j] = busy;
-                j++;
+                _data[busy_count] = other._data[i];
+                _states[busy_count] = busy;
+                busy_count++;
             }
         }
     }
+    
 }
 
 template <class T>
@@ -150,6 +152,55 @@ inline T* TVector<T>::data() noexcept {
     return _data;
 }
 
+template <class T>
+inline T& TVector<T>::front() {
+    if (is_empty()) throw std::out_of_range("Vector is empty");
+    size_t it = real_pos(0);
+    return _data[it];
+}
+
+template <class T>
+inline T& TVector<T>::back() {
+    if (is_empty()) throw std::out_of_range("Vector is empty");
+    size_t it = real_pos(_size - 1);
+    return _data[it];
+}
+
+template <class T>
+inline T* TVector<T>::begin() noexcept {
+    T* main_address = real_address(0);
+    return main_address;
+}
+
+template <class T>
+inline T* TVector<T>::end() noexcept {
+    T* main_address = nullptr;
+    if (!is_empty()) {
+        main_address = (real_address(_size - 1) + 1);
+    }
+    return main_address;
+}
+
+template <class T>
+void TVector<T>::push_front(const T& value) {
+    reallocate(_size + 1);
+    _size += 1;
+    for (size_t i = _size - 1; i > 0; i--) {
+        _data[i] = _data[i - 1];
+        _states[i] = _states[i - 1];
+    }
+    _data[0] = value;
+    _states[0] = busy;
+}
+
+template <class T>
+void TVector<T>::push_back(const T& value) {
+    reallocate(_size + 1);
+    _size += 1;
+    _data[_size - 1] = value;
+    _states[_size - 1] = busy;
+}
+
 template<class T>
 inline const T* TVector<T>::data() const noexcept {
     return _data;
@@ -165,18 +216,61 @@ inline size_t TVector<T>::capacity() const noexcept {
     return _capacity;
 }
 
+template <class T>
+inline const T& TVector<T>::front() const {
+    if (is_empty()) throw std::out_of_range("Vector is empty");
+    size_t it = real_pos(0);
+    return _data[it];
+}
+
+template <class T>
+inline const T& TVector<T>::back() const {
+    if (is_empty()) throw std::out_of_range("Vector is empty");
+    size_t it = real_pos(_size - 1);
+    return _data[it];
+}
+
+template <class T>
+inline const T* TVector<T>::begin() const noexcept {
+    T* main_address = real_address(_data, 0);
+    return main_address;
+}
+
+template <class T>
+inline const T* TVector<T>::end() const noexcept {
+    T* main_address = nullptr;
+    if (!is_empty()) {
+        main_address = (real_address(_size - 1) + 1);
+    }
+    return main_address;
+}
+
 template<class T>
 const T& TVector<T>::operator[](size_t pos) const {
     if (pos >= _size) {
-        throw std::logic_error("Index out of range");
+        throw std::out_of_range("Index out of range");
     }
+    size_t it = real_pos(pos);
+    return _data[it];
+}
 
+template<class T>
+T& TVector<T>::operator[](size_t pos) {
+    if (pos >= _size) {
+        throw std::out_of_range("Index out of range");
+    }
+    size_t it = real_pos(pos);
+    return _data[it];
+}
+
+template<class T>
+size_t TVector<T>::real_pos(size_t pos) {
     size_t busy_count = 0;
     for (size_t i = 0; i < _size; i++) {
         if (_states[i] == busy) {
             if (busy_count == pos)
             {
-                return _data[i];
+                return i;
             }
             busy_count++;
         }
@@ -184,8 +278,47 @@ const T& TVector<T>::operator[](size_t pos) const {
 }
 
 template<class T>
-T& TVector<T>::operator[](size_t pos) {
-    return const_cast<T&>(std::as_const(*this)[pos]);
+T* TVector<T>::real_address(size_t pos) {
+    size_t busy_count = 0;
+    for (size_t i = 0; i < _size; i++) {
+        if (_states[i] == busy) {
+            if (busy_count == pos)
+            {
+                return &_data[i];
+            }
+            busy_count++;
+        }
+    }
+    return nullptr;
+}
+
+template<class T>
+void TVector<T>::reallocate(size_t new_size) {
+    if (new_size <= _capacity) return;
+    size_t new_capacity = (new_size / STEP_OF_CAPACITY + 1) * STEP_OF_CAPACITY;
+
+    T* new_data = new T[new_capacity];
+    State* new_states = new State[new_capacity];
+
+    size_t busy_count = 0;
+    for (size_t i = 0; i < _capacity; i++) {
+        if (_states[i] == busy) {
+            new_data[busy_count] = _data[i];
+            new_states[busy_count] = busy;
+            busy_count++;
+        }
+    }
+
+    for (size_t i = new_size; i < new_capacity; i++) {
+        new_states[i] = empty;
+    }
+
+    delete[] _data;
+    delete[] _states;
+
+    _data = new_data;
+    _states = new_states;
+    _capacity = new_capacity;
 }
 
 #endif  // ONLINE_GAME_STORE_PROJECT_MY_VECTOR_H_
--- Online_Game_Store_Project/Test_Application/main.cpp
+++ Online_Game_Store_Project/Test_Application/main.cpp
@@ -74,6 +74,13 @@ namespace TestSystem {
     }
 };
 
+template < class T>
+void print(TVector<T> vec_1) {
+    for (size_t i = 0; i < vec_1.size(); i++) {
+        std::cout << vec_1[i] << std::endl;
+    }
+}
+
 bool test_1_try_create_default_vector() {
     bool expected_result = true;
     bool actual_result = true;
@@ -176,6 +183,92 @@ bool test_9_check_is_empty() {
     return TestSystem::check(expected_result, actual_result);
 }
 
+bool test_10_check_front_back_begin_end() {
+    TVector<int> vec_1({ 1000,2,456,777 });
+    bool result = (TestSystem::check(1000, vec_1.front())) && (TestSystem::check(777, vec_1.back())) && 
+        (TestSystem::check(1000, *vec_1.begin())) && (TestSystem::check(777, *(vec_1.end() - 1)));
+    return result;
+}
+
+bool test_11_throw_when_try_use_front_in_empty_vector() {
+    TVector<double> vec_1;
+    bool expected_result = false;
+    bool actual_result = true;
+    try {
+        double error = vec_1.front();
+    }
+    catch (const std::exception& ex) {
+        actual_result = false;
+    }
+    return TestSystem::check(expected_result, actual_result);
+}
+
+bool test_12_throw_when_try_use_back_in_empty_vector() {
+    TVector<double> vec_1;
+    bool expected_result = false;
+    bool actual_result = true;
+    try {
+        double error = vec_1.back();
+    }
+    catch (const std::exception& ex) {
+        actual_result = false;
+    }
+    return TestSystem::check(expected_result, actual_result);
+}
+
+bool test_13_check_begin_and_end_in_empty_vector() {
+    TVector<int> vec_1;
+    bool expected_result = true;
+    bool actual_result = true;
+
+    if (vec_1.begin() != nullptr || vec_1.end() != nullptr) {
+        actual_result = false;
+    }
+    return TestSystem::check(expected_result, actual_result);
+}
+
+bool test_14_check_push_front() {
+    TVector<char> vec_1({ '$','2','3' });
+    vec_1.push_front('5');
+    bool result = (TestSystem::check((size_t)4, vec_1.size())) && (TestSystem::check((size_t)15, vec_1.capacity())) &&
+        (TestSystem::check('5', vec_1.front())) && (TestSystem::check('3', vec_1.back())) &&
+        (TestSystem::check('$', vec_1[1])) && (TestSystem::check('2',vec_1[2]));
+    return result;
+}
+
+bool test_15_check_push_front_in_empty_vector() {
+    TVector<int> vec_1({});
+    vec_1.push_front(2000);
+    bool result = (TestSystem::check((size_t)1, vec_1.size())) && (TestSystem::check((size_t)15, vec_1.capacity())) &&
+        (TestSystem::check(2000, vec_1.front())) && (TestSystem::check(2000, vec_1.back()));
+    return result;
+}
+
+bool test_16_check_push_back() {
+    TVector<int> vec_1({ 1, 2, 300 });
+    vec_1.push_back(4000);
+    bool result = (TestSystem::check((size_t)4, vec_1.size())) && (TestSystem::check((size_t)15, vec_1.capacity())) &&
+        (TestSystem::check(1, vec_1.front())) && (TestSystem::check(4000, vec_1.back())) &&
+        (TestSystem::check(2, vec_1[1])) && (TestSystem::check(300, vec_1[2]));
+    return result;
+}
+
+bool test_17_check_push_back_in_empty_vector() {
+    TVector<float> vec_1;
+    vec_1.push_back(2.0f);
+    bool result = (TestSystem::check((size_t)1, vec_1.size())) && (TestSystem::check((size_t)15, vec_1.capacity())) &&
+        (TestSystem::check(2.0f, vec_1.front())) && (TestSystem::check(2.0f, vec_1.back()));
+    return result;
+}
+
+bool test_18_check_several_push() {
+    TVector<int> vec_1(15);
+    vec_1.push_back(23);
+    vec_1.push_front(22);
+    bool result = (TestSystem::check((size_t)17, vec_1.size())) && (TestSystem::check((size_t)30, vec_1.capacity())) &&
+        (TestSystem::check(22, vec_1.front())) && (TestSystem::check(23, vec_1.back()));
+    return result;
+}
 int main() {
     User user_1;
     Client client_1;
@@ -196,6 +289,19 @@ int main() {
     TestSystem::start_test(test_7_try_create_vector_with_copy, " TVector.test_7_try_create_vector_with_copy");
     TestSystem::start_test(test_8_check_values_in_copy, " TVector.test_8_check_values_in_copy");
     TestSystem::start_test(test_9_check_is_empty, " TVector.test_9_check_is_empty");
+    TestSystem::start_test(test_10_check_front_back_begin_end, " TVector.test_10_check_front_back_begin_end");
+    TestSystem::start_test(test_11_throw_when_try_use_front_in_empty_vector,
+        " TVector.test_11_throw_when_try_use_front_in_empty_vector");
+    TestSystem::start_test(test_12_throw_when_try_use_back_in_empty_vector,
+        " TVector.test_12_throw_when_try_use_back_in_empty_vector");
+    TestSystem::start_test(test_13_check_begin_and_end_in_empty_vector,
+        " TVector.test_13_check_begin_and_end_in_empty_vector");
+    TestSystem::start_test(test_14_check_push_front," TVector.test_14_check_push_front");
+    TestSystem::start_test(test_15_check_push_front_in_empty_vector, 
+        " TVector.test_15_check_push_front_in_empty_vector");
+    TestSystem::start_test(test_16_check_push_back, " TVector.test_16_check_push_back");
+    TestSystem::start_test(test_17_check_push_back_in_empty_vector, " TVector.test_17_check_push_back_in_empty_vector");
+    TestSystem::start_test(test_18_check_several_push, " TVector.test_18_check_several_push");
 
     TestSystem::print_final_info();
     system("pause");
